628f0b215b3a96330339da9113ad93009fbb9ab4
Bodia3721 <43144801+Bodia3721@users.noreply.github.com>
2022-01-25 18:04:28 +0200

    Create main.py

---
+++ Calculator/main.py
@@ -0,0 +1,16 @@
+print("Select the action you want to do: +, -, *, /")
+action = input("Action:")
+
+if action in ('+', '-', '*', '/',):
+    x = float(input("x = "))
+    y = float(input("y = "))
+    if action == '+':
+        print('x + y =', x+y)
+    elif action == '-':
+        print('x - y =', x - y)
+    elif action == '*':
+        print('x * y =', x * y)
+    elif action == '/':
+        if y != 0:
+            print('x / y =', x / y)
+        else: print("Can't divide by 0")
