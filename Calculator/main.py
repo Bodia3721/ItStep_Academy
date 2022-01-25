910d3f9cdc50f7787133361e187f49a059429df3
Bodia3721 <43144801+Bodia3721@users.noreply.github.com>
2022-01-25 17:18:06 +0200

    Update main.py

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+print("Select the action you want to do: +, -, /, * ")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+action = input("Action:")
+if action in ('+', '-', '*', '/'):
+    x = float(input('x = '))
+    y = float(input('y = '))
 
+    if action == '+':
+        print('x + y =', x+y)
+    elif action == '-':
+        print('x - y =', x-y)
+    elif action == '*':
+        print('x * y =', x*y)
+    elif action == '/':
+        if y != 0:
+            print('x / y =', x / y)
+        else:
+            print("Can't divide by 0")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
