04d3d2ceaa575b1388936ebe0b931e127d81df38
Bodia3721 <43144801+Bodia3721@users.noreply.github.com>
2022-02-10 12:57:20 +0200

    Create conditional_hw_2.py

---
+++ Conditionals and loops/conditional_hw_2.py
@@ -0,0 +1,73 @@
+#---------------Домашнее задание--Задание №1-----------------
+# day = int(input('Enter the number of the day: '))
+# if day == 1:
+#     print('Monday')
+# elif day == 2:
+#     print('Tuesday')
+# elif day == 3:
+#     print('Wednesday')
+# elif day == 4:
+#     print('Thursday')
+# elif day == 5:
+#     print('Friday')
+# elif day == 6:
+#     print('Saturday')
+# elif day == 7:
+#     print('Sunday')
+# else:
+#     print('Error')
+
+#---------------Домашнее задание--Задание №2-----------------
+
+# month = int(input('Enter the number of the month: '))
+# if month == 1:
+#     print('January')
+# elif month == 2:
+#     print('February')
+# elif month == 3:
+#     print('March')
+# elif month == 4:
+#     print('April')
+# elif month == 5:
+#     print('May')
+# elif month == 6:
+#     print('June')
+# elif month == 7:
+#     print('July')
+# elif month == 8:
+#     print('August')
+# elif month == 9:
+#     print('September')
+# elif month == 10:
+#     print('October')
+# elif month == 11:
+#     print('November')
+# elif month == 12:
+#     print('December')
+# else:
+#     print('Error')
+
+#---------------Домашнее задание--Задание №3-----------------
+
+# number = int(input('Enter the number: '))
+# if number > 0:
+#     print('Number is positive')
+# elif number == 0:
+#     print('Number is equal to zero')
+# else:
+#     print('Number is negative')
+
+#---------------Домашнее задание--Задание №4-----------------
+
+# num1 = int(input('Enter the first number: '))
+# num2 = int(input('Enter the second number: '))
+#
+# if num1 == num2:
+#     print(num1, '=', num2)
+# elif num1 > num2:
+#     print(num2, num1)
+# else:
+#     print(num1, num2)
+
+
+
